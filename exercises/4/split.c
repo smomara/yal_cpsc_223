7e2cb1067eb01bc19dd21a0f827d55ce552e9424
smomara <omarams@cua.edu>
2024-02-27 14:16:38 -0500

    update

--- exercises/4/split.c
+++ exercises/4/split.c
@@ -39,3 +39,42 @@ char *join(char **a, int c) {
 
     return output;
 }
+
+char **split(const char *s, int c) {
+    size_t count = 0;
+    for (const char *p = s; *p; p++) {
+        if (*p == c) {
+            count++;
+        }
+    }
+
+    size_t n = count+1;
+    char **pointers = calloc(n+1, sizeof(char *));
+    char *data = strdup(s);
+    size_t top = 0;
+    char *tail = data;
+
+    for (;;) {
+        pointers[top++] = tail;
+
+        char *next = strchr(tail, c);
+
+        if (c == '\0' || next == 0) {
+            break;
+        } else {
+            *next = '\0';
+            tail = next + 1;
+        }
+    }
+
+    assert(top == n);
+
+    pointers[top] = 0;
+
+    return pointers;
+}
+
+void freeSplit(char **a) {
+    free(a[0]);
+    free(a);
+}
